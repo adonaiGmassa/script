0389f1af1cdae33f02ce27b7c94c27c968834c22
Massa Adonaï <167198345+adonaiGmassa@users.noreply.github.com>
2024-11-30 19:54:19 +0100

    change testUnitaire

--- PowerShell/Automat/automatisationTestUnitaire.ps1
+++ PowerShell/Automat/automatisationTestUnitaire.ps1
@@ -9,13 +9,14 @@ if (-Not (Test-Path $projectPath)) {
 }
 
 # Naviguer vers le répertoire du projet
-cd $projectPath
+Set-Location -Path $projectPath
 
 # Exécuter les tests unitaires avec dotnet test et capturer les résultats dans un fichier
 try {
+    # Exécution de la commande dotnet test et capture du résultat
     $testResult = dotnet test --no-build --logger "trx;LogFileName=test_results.trx"
     
-    # Vérifier si la commande dotnet test a réussi
+    # Vérifier si la commande dotnet test a renvoyé un message de succès ou d'échec
     if ($testResult -match "Total tests: (\d+), Passed: (\d+), Failed: (\d+)") {
         $totalTests = [int]$matches[1]
         $passedTests = [int]$matches[2]
@@ -23,6 +24,7 @@ try {
         
         # Afficher les résultats des tests
         Write-Host "$passedTests tests réussis sur $totalTests"
+        
         if ($failedTests -gt 0) {
             Write-Host "Certains tests ont échoué : $failedTests tests échoués."
             exit 1  # Si des tests échouent, retourner un code d'erreur non nul
